c8efae0cf4feeeaa2ca889fe5f814868d04ecf78
一样有你 <fan@fans-MacBook-Air.local>
2017-05-16 09:30:22 +0800

    2

--- NewProject/ViewController.m
+++ NewProject/ViewController.m
@@ -7,11 +7,9 @@
 //
 
 #import "ViewController.h"
-<<<<<<< HEAD
-#import "FKDLabel.h"
-=======
 
->>>>>>> 49a1b884cb0fb74a1abbb4bed6dd49cb28139b36
+#import "FKDLabel.h"
+
 @interface ViewController ()
 
 @end
@@ -20,22 +18,36 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-<<<<<<< HEAD
+
     
+//    FKDLabel *label = [[FKDLabel alloc] init];
     
-    NSLog(@"123456");
+    [self separaterImageForViewLayer];
     
-    FKDLabel *label = [[FKDLabel alloc] init];
-=======
-    // Do any additional setup after loading the view, typically from a nib.
->>>>>>> 49a1b884cb0fb74a1abbb4bed6dd49cb28139b36
 }
 
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+//layer层分割图片效果
+- (void)separaterImageForViewLayer {
+    UIImage *image = [UIImage imageNamed:@"beautiful.jpeg"];
+    
+    UIView *view1 = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 100, 100)];
+    UIView *view2 = [[UIView alloc] initWithFrame:CGRectMake(150, 0, 100, 100)];
+    UIView *view3 = [[UIView alloc] initWithFrame:CGRectMake(0, 150, 100, 100)];
+    UIView *view4 = [[UIView alloc] initWithFrame:CGRectMake(150, 150, 100, 100)];
+    [self.view addSubview:view1];
+    [self.view addSubview:view2];
+    [self.view addSubview:view3];
+    [self.view addSubview:view4];
+    
+    [self addSpriteImage:image withContentRect:CGRectMake(0, 0, 0.5, 0.5) toLayer:view1.layer];
+    [self addSpriteImage:image withContentRect:CGRectMake(0.5, 0, 0.5, 0.5) toLayer:view2.layer];
+    [self addSpriteImage:image withContentRect:CGRectMake(0, 0.5, 0.5, 0.5) toLayer:view3.layer];
+    [self addSpriteImage:image withContentRect:CGRectMake(0.5, 0.5, 0.5, 0.5) toLayer:view4.layer];
 }
-
+- (void)addSpriteImage:(UIImage *)image withContentRect:(CGRect)rect toLayer:(CALayer *)layer {
+    layer.contents = (__bridge id)image.CGImage;
+    layer.contentsGravity = kCAGravityResizeAspect;
+    layer.contentsRect = rect;
+}
 
 @end
