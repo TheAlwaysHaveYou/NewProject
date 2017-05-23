4b6f6c2b9a0e55710a0cf14c483a82268ed36344
一样有你 <fan@fans-MacBook-Air.local>
2017-05-23 10:46:12 +0800

    添加

---
+++ NewProject/SecondController.h
@@ -0,0 +1,13 @@
+//
+//  SecondController.h
+//  NewProject
+//
+//  Created by fan on 17/5/22.
+//  Copyright © 2017年 fan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondController : UIViewController
+
+@end
